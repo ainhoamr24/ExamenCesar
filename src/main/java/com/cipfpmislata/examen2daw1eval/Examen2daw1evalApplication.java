2c89f56f307f7b94a2507647fe950faf1bcb7f51
CESAR GUIJARRO ROSALENY <cguijarro@0A6PC04L.ad.fpmislata.com>
2025-11-19 10:43:19 +0100

    examen

---
+++ src/main/java/com/cipfpmislata/examen2daw1eval/Examen2daw1evalApplication.java
@@ -0,0 +1,13 @@
+package com.cipfpmislata.examen2daw1eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen2daw1evalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen2daw1evalApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/V1__create_tables.sql
@@ -0,0 +1,40 @@
+CREATE TABLE `actors` (
+                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                          `name` varchar(255) DEFAULT NULL,
+                          `birth_year` int(4) DEFAULT NULL,
+                          `death_year` int(4) DEFAULT NULL,
+                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1770 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+CREATE TABLE `directors` (
+                             `id` int(11) NOT NULL AUTO_INCREMENT,
+                             `name` varchar(255) DEFAULT NULL,
+                             `birth_year` int(4) DEFAULT NULL,
+                             `death_year` int(4) DEFAULT NULL,
+                             PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=138 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+CREATE TABLE `movies` (
+                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                          `title` varchar(57) DEFAULT NULL,
+                          `year` int(4) DEFAULT NULL,
+                          `image` varchar(161) DEFAULT NULL,
+                          `runtime` int(4) DEFAULT NULL,
+                          `description` varchar(255) DEFAULT NULL,
+                          `director_id` int(11) DEFAULT NULL,
+                          PRIMARY KEY (`id`),
+                          KEY `movies_FK` (`director_id`),
+                          CONSTRAINT `movies_FK` FOREIGN KEY (`director_id`) REFERENCES `directors` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=223 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+CREATE TABLE `actors_movies` (
+                                 `id` int(11) NOT NULL AUTO_INCREMENT,
+                                 `movie_id` int(11) NOT NULL,
+                                 `actor_id` int(11) NOT NULL,
+                                 `characters` varchar(255) DEFAULT NULL,
+                                 PRIMARY KEY (`id`),
+                                 KEY `actors_movies_FK` (`actor_id`),
+                                 KEY `actors_movies_FK2` (`movie_id`),
+                                 CONSTRAINT `actors_movies_FK` FOREIGN KEY (`actor_id`) REFERENCES `actors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+                                 CONSTRAINT `actors_movies_FK2` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=907 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
