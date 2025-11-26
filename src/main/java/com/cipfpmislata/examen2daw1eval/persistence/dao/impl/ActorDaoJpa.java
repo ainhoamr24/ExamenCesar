a3d7cfe8fda561e647e497310e4c4dc6b5a84532
ainhoamr24 <ainhoamiret@gmail.com>
2025-11-26 10:23:34 +0100

    Implement JPA DAO classes with repository support

--- src/main/java/com/cipfpmislata/examen2daw1eval/persistence/dao/impl/ActorDaoJpa.java
+++ src/main/java/com/cipfpmislata/examen2daw1eval/persistence/dao/impl/ActorDaoJpa.java
@@ -1,6 +1,17 @@
 package com.cipfpmislata.examen2daw1eval.persistence.dao.impl;
 
 import com.cipfpmislata.examen2daw1eval.persistence.dao.ActorDao;
+import com.cipfpmislata.examen2daw1eval.persistence.dao.impl.entity.ActorJpaEntity;
+import jakarta.persistence.EntityManager;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-public class ActorDaoJpa implements ActorDao {
+@Repository
+@Transactional(readOnly = true)
+public class ActorDaoJpa extends SimpleJpaRepository<ActorJpaEntity, Long> implements ActorDao {
+
+    public ActorDaoJpa(EntityManager entityManager) {
+        super(ActorJpaEntity.class, entityManager);
+    }
 }
--- src/main/java/com/cipfpmislata/examen2daw1eval/persistence/dao/impl/DirectorDaoJpa.java
+++ src/main/java/com/cipfpmislata/examen2daw1eval/persistence/dao/impl/DirectorDaoJpa.java
@@ -1,6 +1,17 @@
 package com.cipfpmislata.examen2daw1eval.persistence.dao.impl;
 
 import com.cipfpmislata.examen2daw1eval.persistence.dao.DirectorDao;
+import com.cipfpmislata.examen2daw1eval.persistence.dao.impl.entity.DirectorJpaEntity;
+import jakarta.persistence.EntityManager;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-public class DirectorDaoJpa implements DirectorDao {
+@Repository
+@Transactional(readOnly = true)
+public class DirectorDaoJpa extends SimpleJpaRepository<DirectorJpaEntity, Long> implements DirectorDao {
+
+    public DirectorDaoJpa(EntityManager entityManager) {
+        super(DirectorJpaEntity.class, entityManager);
+    }
 }
